89f7754858dc4bdb0ca6026f767b4daafa47d877
joowonhyun <78545341+joowonhyun@users.noreply.github.com>
2025-02-03 23:52:16 +0900

    feat: add common chore

---
+++ src/components/_core/space.ts
@@ -0,0 +1,14 @@
+const space = {
+  none: "0px",
+  s4: "4px",
+  s8: "8px",
+  s16: "16px",
+  s20: "20px",
+  s24: "24px",
+  s32: "32px",
+  s40: "40px",
+  s48: "48px",
+  s64: "64px",
+};
+
+export default space;
---
+++ src/components/_core/sprinkles.css.ts
@@ -0,0 +1,65 @@
+import { defineProperties, createSprinkles } from "@vanilla-extract/sprinkles";
+import colors from "./palate";
+import { fontSize, fontWeight } from "./typo";
+import space from "./space";
+
+const responsiveProperties = defineProperties({
+  conditions: {
+    mobile: {},
+    tablet: { "@media": "screen and (min-width: 768px)" },
+    desktop: { "@media": "screen and (min-width: 1024px)" },
+  },
+  defaultCondition: "mobile",
+  properties: {
+    position: ["relative", "absolute", "fixed", "sticky"],
+    display: ["none", "flex", "block", "inline"],
+    flexDirection: ["row", "column"],
+    justifyContent: [
+      "stretch",
+      "flex-start",
+      "center",
+      "flex-end",
+      "space-around",
+      "space-between",
+    ],
+    alignItems: ["stretch", "flex-start", "center", "flex-end"],
+    borderRadius: space,
+    paddingTop: space,
+    paddingBottom: space,
+    paddingLeft: space,
+    paddingRight: space,
+    marginTop: space,
+    marginBottom: space,
+    marginLeft: space,
+    marginRight: space,
+    gap: space,
+    flexWrap: ["nowrap", "wrap"],
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    // etc.
+  },
+  shorthands: {
+    padding: ["paddingTop", "paddingBottom", "paddingLeft", "paddingRight"],
+    paddingX: ["paddingLeft", "paddingRight"],
+    paddingY: ["paddingTop", "paddingBottom"],
+    margin: ["marginTop", "marginBottom", "marginLeft", "marginRight"],
+    marginX: ["marginLeft", "marginRight"],
+    marginY: ["marginTop", "marginBottom"],
+    placeItems: ["justifyContent", "alignItems"],
+  },
+});
+
+const colorProperties = defineProperties({
+  conditions: {
+    lightMode: {},
+    darkMode: { "@media": "(prefers-color-scheme: dark)" },
+  },
+  defaultCondition: "lightMode",
+  properties: {
+    color: colors,
+    background: colors,
+  },
+});
+
+export const sprinkles = createSprinkles(responsiveProperties, colorProperties);
+export type Sprinkles = Parameters<typeof sprinkles>[0];
